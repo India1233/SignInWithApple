8bc1815acd5e1dc7119bb24cbfb7c782d33a276d
shigas <shigasuresh546@gmail.com>
2019-11-20 21:31:40 +0530

    SIgnInWithApple

--- SignInWithApple/ViewController.swift
+++ SignInWithApple/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+import AuthenticationServices
 
 class ViewController: UIViewController {
-
+    
+    let button = ASAuthorizationAppleIDButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        button.addTarget(self, action: #selector(handleAppleIDAuthorization), for: .touchUpInside)
+        button.frame.size.width = 300
+        button.frame.size.height = 60
+        button.center = self.view.center
+        //CGRect(origin:self.view.center, size: CGSize(width: 300, height: 60))
+        
+        self.view.addSubview(button)
     }
 
+    @objc func handleAppleIDAuthorization() {
+        
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        controller.delegate = self
+        controller.presentationContextProvider = self as? ASAuthorizationControllerPresentationContextProviding
+        controller.performRequests()
+        
+        
+    }
 
 }
 
+extension ViewController: ASAuthorizationControllerDelegate{
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        
+        switch authorization.credential {
+        case let credentials as ASAuthorizationAppleIDCredential:
+           
+            let userId = credentials.user
+            print("User Identifier: ", userId)
+            
+            if let fullName = credentials.fullName {
+                print(fullName)
+            }
+            
+            if let emailId = credentials.email {
+                print(emailId)
+            }
+            break
+        default:
+            break
+        }
+    }
+    
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        print("Apple ID Authorization failed. ", error.localizedDescription)
+    }
+}
+
+
+extension ViewController: ASAuthorizationControllerPresentationContextProviding {
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return view.window!
+    }
+}
